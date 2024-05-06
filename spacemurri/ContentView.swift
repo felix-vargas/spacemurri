6e167415bec30730204ef140ead171cea77c5997
Felix <felix.vargas@sansano.usm.cl>
2024-05-06 13:35:07 -0400

    Initial Commit

---
+++ spacemurri/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  spacemurri
+//
+//  Created by Felix Vargas on 06-05-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ spacemurri/spacemurriApp.swift
@@ -0,0 +1,17 @@
+//
+//  spacemurriApp.swift
+//  spacemurri
+//
+//  Created by Felix Vargas on 06-05-24.
+//
+
+import SwiftUI
+
+@main
+struct spacemurriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
